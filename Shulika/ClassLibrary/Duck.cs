f23fdbd96f6d7ab33ee4d472087d2c2628ca7e5d
JollyFoxy <pavel.mizirev@gmail.com>
2022-01-21 15:13:16 +0300

    Версия 0.1

---
+++ Shulika/ClassLibrary/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public abstract class Duck
+    {
+        public string Quack()
+        {
+            return "Quac " + this.GetType();
+        }
+
+        public string Swim()
+        {
+            return "Swim " + this.GetType();
+        }
+
+        public abstract string Display();
+    }
+}
---
+++ Shulika/Shulika/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Shulika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck duck1 = new MallardDuck();
+            RedheadDuck duck2 = new RedheadDuck();
+
+            Duck[] array = new Duck[] { duck1, duck2 };
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(array[i].Quack());
+                Console.WriteLine(array[i].Swim());
+                Console.WriteLine(array[i].Display());
+            }
+            Console.ReadKey();
+        }
+    }
+}
